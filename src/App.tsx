6b9e4590cdc59e97ff33a2ff3c03e0351a4c41b6
Niall-Roche <niallroche17@gmail.com>
2026-07-07 10:16:13 +0100

    Initial commit: React DnD playground with dndkit vertical sortable list

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { SortableList } from './components/SortableList'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Drag & Drop List</h1>
+      <SortableList />
+    </div>
+  )
+}
+
+export default App
